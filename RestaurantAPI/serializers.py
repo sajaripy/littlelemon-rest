3e110dd957f80f32ddb5e3f6d639ebbd5eb82b93
sajari <sajari.k.14@gmail.com>
2024-01-17 06:01:26 +0530

    learned different renderers

--- RestaurantAPI/serializers.py
+++ RestaurantAPI/serializers.py
@@ -10,11 +10,16 @@ class CategorySerializer(serializers.ModelSerializer):
 class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
-    # category = CategorySerializer()
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
     class Meta:
         model = MenuItem
-        fields = ['id','title','price','stock','price_after_tax','category']
-        depth = 1
+        fields = ['id','title','price','stock','price_after_tax','category','category_id']
+        # depth = 1
+        extra_kwargs = {
+            'price': {'min_value': 2},
+            'stock': {'min_value': 1}
+        }
     
     def calculate_tax(self, product:MenuItem):
         return product.price * Decimal(1.1)
--- RestaurantAPI/views.py
+++ RestaurantAPI/views.py
@@ -1,9 +1,27 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import MenuItem, Category
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from django.shortcuts import get_object_or_404
 from .serializers import MenuItemSerializer, CategorySerializer
+
+from rest_framework.renderers import TemplateHTMLRenderer, StaticHTMLRenderer
+from rest_framework_csv.renderers import CSVRenderer
+from rest_framework_yaml.renderers import YAMLRenderer
+
+@api_view() 
+@renderer_classes([TemplateHTMLRenderer])
+def menu(request):
+    items = MenuItem.objects.select_related('category').all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response({'data':serialized_item.data}, template_name='menu-items.html')
+
+@api_view(['GET'])
+@renderer_classes([StaticHTMLRenderer])
+def welcome(request):
+    data = '<html><body><h1>Welcome To Little Lemon API Project</h1></body></html>'
+    return Response(data)
+
 
 @api_view()
 def category_detail(request, pk):
@@ -11,10 +29,47 @@ def category_detail(request, pk):
     serialized_category = CategorySerializer(category)
     return Response(serialized_category.data)
 
-class MenuItemsView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.select_related('category').all()
-    serializer_class = MenuItemSerializer
+# class MenuItemsView(generics.ListCreateAPIView):    
+#     def list(self, request):
+#         if request.method == 'GET':
+#             queryset = MenuItem.objects.select_related('category').all()
+#             serializer_class = MenuItemSerializer(queryset, many=True)
+#             return Response(serializer_class.data)
+#     def create(self, request):
+#         if request.method == 'POST':
+#             serializer_class = MenuItemSerializer(data=request.data)
+#             serializer_class.is_valid(raise_exception=True)
+#             serializer_class.save()
+#             return Response(serializer_class.data, status.HTTP_201_CREATED)
 
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+# class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = MenuItem.objects.all()
+#     serializer_class = MenuItemSerializer
+
+
+# from django.shortcuts import render
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+
+# Create your views here.
+@api_view(['GET','POST'])
+# @renderer_classes([CSVRenderer])
+# @renderer_classes([YAMLRenderer])
+def menu_items(request):
+    if request.method == 'GET':
+        items = MenuItem.objects.select_related('category').all()
+        serialized_item = MenuItemSerializer(items, many=True)
+        return Response(serialized_item.data)
+    if request.method == 'POST':
+        serialized_item = MenuItemSerializer(data=request.data)
+        serialized_item.is_valid(raise_exception=True)
+        serialized_item.save()
+        return Response(serialized_item.data, status.HTTP_201_CREATED)
+
+@api_view()
+def single_item(request, id):
+    item = get_object_or_404(MenuItem,pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
